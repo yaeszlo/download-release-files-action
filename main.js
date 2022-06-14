6ab26a317446e5567143cc6cafc736eaf715691c
yaeszlo <laszlo@cryptonovae.com>
2022-06-14 21:11:27 +0200

    Implement download files

--- main.js
+++ main.js
@@ -1,46 +1,55 @@
 const {getOctokit} = require("@actions/github");
+const {pipeline} = require("stream/promises");
+const {createWriteStream} = require("fs");
+const {cwd} = require('node:process');
 const core = require('@actions/core');
 const fetch = require("node-fetch");
 
 const token = core.getInput('token');
 const repository = core.getInput('repository');
+const releaseName = core.getInput('release_name');
+const fileName = core.getInput('file_name');
 
 const octokit = getOctokit(token);
+const workingDir = cwd();
 
-async function start() {
-  const [owner, repo] = repository.split('/');
-
-  console.log(repository);
-  console.log(repo);
-  console.log(owner);
-  console.log(token.length);
-
+async function run() {
   try {
-    const releases = await octokit.rest.repos.listReleases({
-      repo: repo,
-      owner: owner,
-      per_page: 100,
-    });
-    console.log('=====================direct assets==================')
-    console.log(releases.data[0].assets);
+    const releases = await getRepositoryReleases(repository);
+    const foundRelease = findRelease(releases, releaseName);
 
-
-    console.log('==============================URL ASSETS=============================')
-    const assets = await request(releases.data[0].assets_url);
-    console.log(assets);
+    await findAndDownloadReleaseAssets(foundRelease);
   } catch (e) {
     console.log(e);
   }
-
 }
 
-async function request(url) {
-  const response = await fetch(url, {
-    headers: {
-      Authorization: `token ${token}`
-    }
+async function findAndDownloadReleaseAssets(release, fileName) {
+  const foundAssets = release.assets.filter(asset => asset.name.match(fileName));
+  return Promise.all(foundAssets.map(downloadAsset));
+}
+
+function findRelease(releases, releaseName) {
+  if (!releaseName) {
+    return releases.find(release => release.latest);
+  }
+  return releases.find(release => release.name.match(releaseName));
+}
+
+async function getRepositoryReleases(repository) {
+  const [owner, repo] = repository.split('/');
+
+  return octokit.rest.repos.listReleases({
+    repo: repo,
+    owner: owner,
+    per_page: 100,
   });
-  return response.json();
 }
 
-start();
+async function downloadAsset(asset) {
+  const assetName = asset.name;
+  const assetUrl = asset.url;
+  return pipeline((await fetch(assetUrl)).body, createWriteStream(`${workingDir}/${assetName}`));
+}
+
+run();
