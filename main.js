09ef4b5c2ebd4862ec84bc123a5f36a357e98160
yaeszlo <laszlo@cryptonovae.com>
2022-06-14 17:57:56 +0200

    Log values, Reformat inputs, Remove unused imports

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
-const github = require("@actions/github");
-const {getOctokit, context} = require("@actions/github");
+const {getOctokit} = require("@actions/github");
 const core = require('@actions/core');
 
 const token = core.getInput('token');
 const repository = core.getInput('repository');
-const owner = core.getInput('owner');
 const resultsPerPage = +core.getInput('results_per_page');
 const page = +core.getInput('page');
 
-const octokit = getOctokit(github.token);
+const octokit = getOctokit(token);
 
 async function start() {
+  const [ repo, owner ] = repository.split('/')
+
+  console.log(repository);
+  console.log(repo);
+  console.log(owner);
+  console.log(token.length);
+
   const releases = await octokit.repos.listReleases({
-    repo: repository ?? context.repo.repo,
-    owner: owner ?? context.repo.owner,
+    repo: repo,
+    owner: owner,
     per_page: resultsPerPage,
     page: page
   });
